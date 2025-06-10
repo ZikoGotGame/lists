28dd217f2f39739bf56b9f7e53f547f682402763
Zac Tawfick <101304267+ZikoGotGame@users.noreply.github.com>
2025-06-09 21:20:05 -0400

    Continued working on a less sucky singly linked stack.

--- src/second.rs
+++ src/second.rs
@@ -17,6 +17,29 @@ impl List {
     }
 
     pub fn push(&mut self, elem: i32) {
-        // Todo
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, None),
+        });
+        self.head = Some(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, None) {
+            None => None,
+            Some(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut current_link = mem::replace(&mut self.head, None);
+        while let Some(mut boxed_node) = current_link {
+            current_link = mem::replace(&mut boxed_node.next, None);
+        }
     }
 }
